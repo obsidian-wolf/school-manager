434938f35c53763aa983cbf1d3710b77f8acf41d
CJ <cj@bluebird.tech>
2024-12-04 13:35:32 +0000

    New login screen

--- web/src/app/login/index.tsx
+++ web/src/app/login/index.tsx
@@ -1,3 +1,4 @@
+import { AtSymbolIcon, KeyIcon } from '@heroicons/react/24/solid';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useLogin } from '~/api/endpoints';
@@ -30,25 +31,48 @@ function LoginPage() {
     };
 
     return (
-        <div>
-            <h1>Login</h1>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="email"
-                    placeholder="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                />
-                <input
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
-                <button type="submit">Login</button>
-            </form>
+        <div className="h-screen bg-stone-100 flex items-center justify-center">
+            <div className="px-4 max-w-full w-80">
+                <h1 className="py-4">
+                    Login to <strong>School Manager</strong>
+                </h1>
+                <form onSubmit={handleSubmit} className="space-y-4">
+                    <label className="input input-bordered flex items-center gap-2">
+                        <AtSymbolIcon className="h-4 text-neutral-600" />
+                        <input
+                            type="email"
+                            className="grow"
+                            placeholder="Email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                        />
+                    </label>
+                    <label className="input input-bordered flex items-center gap-2">
+                        <KeyIcon className="h-4 text-neutral-600" />
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                    </label>
+                    <div className="flex justify-end">
+                        <button
+                            type="submit"
+                            className="btn btn-outline w-full"
+                            disabled={login.isPending}
+                        >
+                            {login.isPending ? (
+                                <span className="loading loading-spinner"></span>
+                            ) : (
+                                'Login'
+                            )}
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }

832a3730f94bd39782ff89b20e602984c6c31f5e
CJ <cj@bluebird.tech>
2024-12-04 13:21:25 +0000

    Fixes

--- web/src/config.ts
+++ web/src/config.ts
@@ -1,4 +1,4 @@
 const PAM_URL = import.meta.env.VITE_PAM_URL || 'http://localhost:3000';
-const API_URL = import.meta.env.VITE_PAM_URL || 'http://localhost:5001';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5001';
 // https://pam-ai-whatsapp-6fd00ff950de.herokuapp.com/
 export { PAM_URL, API_URL };
